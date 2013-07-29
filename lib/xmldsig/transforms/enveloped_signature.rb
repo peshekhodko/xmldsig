028861150ccd491f90fe773de80264dd73139f05
Oleg Minaev <oleg.minaev@gmail.com>
2013-07-29 03:26:13 +0300

    fix for nil

--- lib/xmldsig/transforms/enveloped_signature.rb
+++ lib/xmldsig/transforms/enveloped_signature.rb
@@ -2,7 +2,7 @@ module Xmldsig
   class Transforms < Array
     class EnvelopedSignature < Transform
       def transform
-        node.xpath("descendant::ds:Signature", Xmldsig::NAMESPACES).first.remove
+        node.xpath("descendant::ds:Signature", Xmldsig::NAMESPACES).first.remove if node.xpath("descendant::ds:Signature", Xmldsig::NAMESPACES).first
         node
       end
     end
